0303aaa91f1aef533566fbead078047427cc99d8
vagne <vagnerdw1@gmail.com>
2025-01-23 08:40:44 -0300

    Inicializa projeto com criação do model de Categoria

---
+++ src/main/java/culinaryapi_Menu_Service/CulinaryapiMenuServiceApplication.java
@@ -0,0 +1,13 @@
+package culinaryapi_Menu_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulinaryapiMenuServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulinaryapiMenuServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/culinaryapi_Menu_Service/CulinaryapiMenuServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package culinaryapi_Menu_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulinaryapiMenuServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
